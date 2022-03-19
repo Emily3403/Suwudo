b3cd285bad8def2920067e03983d5b8dad2e83e0
Emily3403 <seebeckemily3403@gmail.com>
2022-03-19 23:21:37 +0100

    Modified: install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ mkdir -p ~/.cache/sudo
 cp /usr/lib/sudo/sudoers.so "$HOME/.cache/sudo/sudoers.so.bak.$(date +%s)"
 ln -fs "$HOME/.cache/sudo/sudoers.so.bak.$(date +%s)" "$HOME/.cache/sudo/sudoers.so.bak"
 
+./suwudo.py
+
 # This wierd construction silences the segfault error
 # https://stackoverflow.com/a/28522034
 sudo cp ./sudoers.so /usr/lib/sudo/sudoers.so &>/dev/null | cat
